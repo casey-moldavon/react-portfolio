3c15884d9772daedd1d6d994e02a816ffdd6c7de
Casey Moldavon <cmoldavon@gmail.com>
2020-02-14 20:06:34 -0800

    Updates

---
+++ precache-manifest.3729a64cd70eec238aadf81bc953b03a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78f9646a579aa5226c5ef05c4131578",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "04d9c361068b4e3a62ba",
+    "url": "/react-portfolio/static/css/main.5b577e07.chunk.css"
+  },
+  {
+    "revision": "811a7e8b74106f289a53",
+    "url": "/react-portfolio/static/js/2.07a2dd95.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-portfolio/static/js/2.07a2dd95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d9c361068b4e3a62ba",
+    "url": "/react-portfolio/static/js/main.b7cf83b0.chunk.js"
+  },
+  {
+    "revision": "fc28123deae08e53ec3a",
+    "url": "/react-portfolio/static/js/runtime-main.ae70f43c.js"
+  },
+  {
+    "revision": "4cc4456358ec8a860e8fe2d01b8e12c5",
+    "url": "/react-portfolio/static/media/Enchanted Land.4cc44563.otf"
+  },
+  {
+    "revision": "d6d9cda9aed697c02d061b119bfe8287",
+    "url": "/react-portfolio/static/media/background-str.d6d9cda9.jpg"
+  },
+  {
+    "revision": "24a4e8594e98d0b5a8b62dfbb1d16c94",
+    "url": "/react-portfolio/static/media/clicky-game.24a4e859.png"
+  },
+  {
+    "revision": "f3637609dc03154267cbcb30609aa93a",
+    "url": "/react-portfolio/static/media/code-quiz.f3637609.png"
+  },
+  {
+    "revision": "b0e9d31c62c51031572fac802bba4b3e",
+    "url": "/react-portfolio/static/media/crew-builder.b0e9d31c.gif"
+  },
+  {
+    "revision": "04134685456b0a0a913d7c7e4ac427bc",
+    "url": "/react-portfolio/static/media/day-planner.04134685.png"
+  },
+  {
+    "revision": "6b40dd45e73c4bf5a741ea71b6811639",
+    "url": "/react-portfolio/static/media/employee-management.6b40dd45.gif"
+  },
+  {
+    "revision": "90658a50094fab2ab8d9c619ba937390",
+    "url": "/react-portfolio/static/media/medieval-avatars.90658a50.png"
+  },
+  {
+    "revision": "ee9178ee570cbfac5924fd8b9f89c603",
+    "url": "/react-portfolio/static/media/nomster.ee9178ee.png"
+  },
+  {
+    "revision": "8293ecdc0ae560d89384daae8132b2d4",
+    "url": "/react-portfolio/static/media/note-taker.8293ecdc.png"
+  },
+  {
+    "revision": "17c0fc7e4ac871c298c2a87cefd4d8af",
+    "url": "/react-portfolio/static/media/password-generator.17c0fc7e.png"
+  },
+  {
+    "revision": "bc18629bfbe734650c0ee820750ee500",
+    "url": "/react-portfolio/static/media/portfolio.bc18629b.png"
+  },
+  {
+    "revision": "16cfa71d6389c20da4f47126c3933b88",
+    "url": "/react-portfolio/static/media/weather-dashboard.16cfa71d.png"
+  },
+  {
+    "revision": "5b1848d72dd28e69df7fb142321c9d16",
+    "url": "/react-portfolio/static/media/workout-tracker.5b1848d7.png"
+  }
+]);
